7db202631f2f88725ae48a59409dff26b1e2d8f0
FerneyPalacio <ferneypalacioh@gmail.com>
2016-06-23 12:36:24 -0500

    modificaciones

--- js/functions.js
+++ js/functions.js
@@ -17,5 +17,5 @@ $(function() {
 
 $('.popup').click(function (event) {
     event.preventDefault();
-    window.open($(this).attr("href"), "popupWindow", "width=400,height=400,scrollbars=yes");
+    window.open($(this).attr("href"), "popupWindow", "width=720,height=460,scrollbars=yes");
 });
